1c49d9917e4b64128a24614b41ce46439f968188
VictorSZETO1 <victor.szeto@stu.ocsb.ca>
2024-09-18 13:01:32 -0400

    working
    Working "finished" version

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,8 @@ public class Main {
 		System.out.println("ok " + name + " on a scale of 1 to 10 how strong are you?");
 		int str = in.nextInt();
 
-		
 		in.nextLine();
-		
+
 		System.out.println("ok " + name + " name a noun");
 		String noun1 = in.nextLine();
 
@@ -39,30 +38,56 @@ public class Main {
 
 		System.out.println("ok " + name + " who is your best friend");
 		String name2 = in.nextLine();
-		
-		System.out.println("ok " + name + "what is a cool verb ending with ing");
+
+		System.out.println("ok " + name + " what is a cool verb ending with ing");
 		String verb = in.nextLine();
 
 		System.out.println("ok " + name + " lastly an animal");
 		String pet = in.nextLine();
 
-
-
-		if (str < 7) {
-			System.out.println("on the way home from school "+name+" stopped by the");
-			System.out.println(noun1+verb1+"and decided to see if they had any "+verb2+noun2);
-			System.out.println("but while "+name+" was looking around he saw his best frind"+name2+verb+"a"+pet);
-			System.out.println("since "+name+"is a solid "+str+"/10 in strength he decides to punch his friend since "+pet+" is his favorite animal however he is too weak and gets knocked out");
-			System.out.println("the last thing "+name+" saw was a "+verb3+noun3);
+		if (str <= 7) {
+			System.out.println("on the way home from school " + name + " stopped by the");
+			System.out.println(verb1 + " " + noun1 + " and decided to see if they had any " + verb2 + " " + noun2);
+			System.out.println("but while " + name + " was looking around he saw his best frind " + name2 + " " + verb
+					+ " a " + pet);
+			System.out.println(
+					"since " + name + " is a solid " + str + "/10 in strength he decides to punch his friend since "
+							+ pet + " is his favorite animal however he is too weak and gets knocked out");
+			System.out.println("the last thing " + name + " saw was a " + verb3 + " " + noun3);
 		}
-		if (str > 7) {
-			System.out.println("ok " + name + " your to strong for the program :( (score to high " + str + "/10");
+		if (str > 10) {
+			System.out.println("ok " + name + " you're to strong for the program :( (score to high " + str + "/10)");
 
 		}
+
+		{
+			if (str > 7) {
+				System.out.println("on the way home from school " + name + " stopped by the");
+				System.out.println(verb1 + " " + noun1 + "and decided to see if they had any " + verb2 + " " + noun2);
+				System.out.println("but while " + name + " was looking around he saw his best frind" + name2 + " "
+						+ verb + " a " + pet);
+				System.out.println("since " + name + " is a solid " + str + "/10 in strength "
+						+ "he decides to punch his friend since " + pet + " is his favorite animal,");
+				System.out.println("Since " + name + " is so Strong He punches " + name2 + " And knockes him out cold");
+				System.out.println(name + " Procedes to mock " + name2 + " Calling Him a " + verb3 + " " + noun3);
+				in.close();
+			}
+		}
+		double randomValue = Math.random();
 		
-		else {
-		
-	}
-		in.close();
+		if (randomValue <0.25) {
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("this message had a 25% of existing you got lucky!!");
+			
+		}else {
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("no secret message :(   "+randomValue);
+		}
+			
 	}
 }
+
